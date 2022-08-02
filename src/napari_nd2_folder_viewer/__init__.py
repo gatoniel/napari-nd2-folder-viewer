b7fc1d024e03dfba20f29c15e1d1a995155c7082
Niklas Netter <niknett@gmail.com>
2022-08-02 12:45:47 +0200

    works fine

--- src/napari_nd2_folder_viewer/__init__.py
+++ src/napari_nd2_folder_viewer/__init__.py
@@ -1,11 +1,7 @@
-
 try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
-from ._widget import ExampleQWidget, example_magic_widget
+from ._widget import LoadWidget
 
-__all__ = (
-    "ExampleQWidget",
-    "example_magic_widget",
-)
+__all__ = ("LoadWidget",)
